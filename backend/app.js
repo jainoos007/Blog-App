f79dba915c2a752ff655aaa589fa22b65f87f7fd
jainoos <ammjainoos@gmail.com>
2025-02-22 02:06:56 +0530

    connect mongoDb

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 const PORT = 7000;
 
+//connecting Database
+mongoose
+  .connect(
+    "mongodb+srv://admin007:admin@321@cluster0.kcgo7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(PORT, () => {
+    console.log("Server is running at port: " + PORT);
+  })
+  .then((e) => console.log("MongoDB coneected"));
+
 app.get("/", (req, res) => {
   console.log("Hello from server");
-});
-
-app.listen(PORT, () => {
-  console.log("Server is running at port: " + PORT);
 });
