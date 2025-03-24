1911e47abd35a00d997191c7ef20427c077517e9
jainoos <ammjainoos@gmail.com>
2025-03-24 08:20:20 +0530

    added layout feature into root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navigation from "./components/common/Navigation";
-import Footer from "./components/common/Footer";
-import Home from "./pages/Home";
 import Login from "./components/Login";
+import Layout from "./components/common/Layout";
 import Signup from "./components/Signup";
+import Home from "./pages/Home";
 function App() {
   return (
-    <>
-      <Navigation />
-      <Router>
+    <Router>
+      <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-      </Router>
-      <Footer />
-    </>
+      </Layout>
+    </Router>
   );
 }
 
