7a992c549080d3fb17c04be897823cb340e36c23
jainoos <ammjainoos@gmail.com>
2025-03-24 05:56:15 +0530

    create footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Blogs />
       {/* <Login /> */}
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center py-4 shadow-2xl bg-[#7c0fb3]">
+      <div className="text-[#C1BFFF]">
+        Built with 💜 by Jainoos | © 2025 All Rights Reserved.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
