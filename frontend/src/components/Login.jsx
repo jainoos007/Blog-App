f3f0f7c6781a4429ff31ef92b66f2564df27144f
jainoos <ammjainoos@gmail.com>
2025-04-01 02:59:19 +0530

    adding logout feature

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "../service/api";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../redux/features/authSlice";
 
--- frontend/src/components/common/Navigation.jsx
+++ frontend/src/components/common/Navigation.jsx
@@ -1,19 +1,39 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/features/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token); // check if user is logged in
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
   return (
     <div className="bg-primary drop-shadow-md">
       <div className="container flex justify-between items-center py-4">
         <a href="/" className="font-bold text-3xl primary cursor-pointer">
           BlogNest
         </a>
-        <a
-          href="/login"
-          className="primary font-semibold cursor-pointer hover:text-[#7c0fb3b7]
-        "
-        >
-          Login
-        </a>
+        {token ? (
+          <button
+            onClick={handleLogout}
+            className="primary font-semibold cursor-pointer hover:text-[#7c0fb3b7]"
+          >
+            Logout
+          </button>
+        ) : (
+          <a
+            href="/login"
+            className="primary font-semibold cursor-pointer hover:text-[#7c0fb3b7]"
+          >
+            Login
+          </a>
+        )}
       </div>
     </div>
   );
