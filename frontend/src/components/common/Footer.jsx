b49daa203adcd8be1fc0e4ef6f7ddffb295b6e4f
jainoos <ammjainoos@gmail.com>
2025-05-26 12:37:06 +0530

    responsive: footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex justify-center items-center py-4 shadow-2xl bg-[#7c0fb3]">
-      <div className="text-[#C1BFFF]">
+      <div className="text-[#C1BFFF] text-xs sm:text-base">
         Built with 💜 by Jainoos | © 2025 All Rights Reserved.
       </div>
     </div>
